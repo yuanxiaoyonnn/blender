8408594a6765580bce90f065fff494804384f764
ruanxiaoyong <602908445@qq.com>
2024-12-31 10:47:01 +0800

    py基础知识3   if判断

---
+++ chapter_2.py
@@ -0,0 +1,20 @@
+#强制类型转换   数学运算符
+samples=64
+
+cube_name='114514'
+
+追踪精度=1.00
+
+# print(type(samples),type(cube_name),type(追踪精度))
+s_samples=str(samples)
+# print (s_samples,type(s_samples))
+i_cubename=int(cube_name)
+去尾_追踪精度=int(追踪精度)
+f_cubename=float(cube_name)
+print (去尾_追踪精度)
+# print(i_cubename,type(i_cubename))
+# print(f_cubename,type(f_cubename))
+
+# print(samples/64)
+# print (cube_name+cube_name)
+# print(cube_name*3)
---
+++ chapter_3.py
@@ -0,0 +1,9 @@
+#if判断
+samples = input("请输入你的渲染采样")
+#samples=int (samples)
+#print(samples,type(samples))
+
+if type(samples) ==type("字符串"):
+    print("你的数据类型是字符串")
+else:
+    pass
